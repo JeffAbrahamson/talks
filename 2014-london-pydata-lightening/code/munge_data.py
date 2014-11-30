0f92bb3872b8c19e251409476d8fdab86b5af3c9
Jeff Abrahamson <jeff@purple.com>
2014-11-30 19:27:35 +0100

    The code and the base data that we are describing.

---
+++ 2014-london-pydata-lightening/code/munge_data.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+
+"""Analyze time data from the Journée Nationale du Huit, 16 Nov 2014, Nantes."""
+
+import datetime
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import sys
+
+def get_data():
+    """Read the data.
+
+    It has been prepared with the shell script get-data."""
+    # Read the data.
+    df = pd.read_csv('resultats-clean-final.txt', delimiter='\t', header=None, names=['index','boat','team','club','captain','time','offset'])
+
+    # Make time and offset be proper dateime.time values.
+    df.time = pd.Series([datetime.time(0, int(val[:2]), int(val[3:5]), int(val[6:])*100000)
+                         for val in df.time], index = df.index)
+    df.offset = pd.Series([datetime.time(0, int(val[:2]), int(val[3:5]), int(val[6:])*100000)
+                           for val in df.offset], index = df.index)
+    return df
+
+def plot_by_club(df, live):
+    """Plot a histogram of number of boats entered by club."""
+    # Histogram of club representation.
+    df.club.value_counts().plot(kind='bar')
+    plt.xlabel("Participation par club")
+    render_plot(8, 6, live, 'jn8-clubs.png')
+
+def plot_by_team(df, live):
+    """Plot a histogram of number of boats entered by team type."""
+    # Histogram of team representation.
+    df.team.value_counts().plot(kind='bar')
+    plt.xlabel("Participation par type d'équipe")
+    render_plot(8, 6, live, 'jn8-teams.png')
+
+def plot_times_by_club(df, live, zero):
+    """Plot the times for each club.
+
+    If zero is True, then set first x-tick at 0 seconds (beginning of race)."""
+    times = df.loc[:, ['time', 'club']]
+    clubs = times.club.unique()
+    fig, ax = plt.subplots(len(clubs), sharex=True, sharey=True)
+    for index, club in enumerate(clubs):
+        these_times = times[times.club == club]
+        ax[index].plot(these_times.time.as_matrix(), np.ones(len(these_times)), 'o')
+        ax[index].set_ylabel(club, rotation='horizontal', labelpad=30)
+        ax[index].set_yticks([])
+        ax[index].set_ybound(0.5, 1.5)
+        ax[index].set_position([0.1, 0.1, 8.0, 1.0])
+        ax[index].set_yticks([])
+    ax[0].set_title('Temps vu par club')
+    min_tick = ax[0].get_xticks().min()
+    if zero:
+        min_tick = 0.0
+        ax[0].set_xbound(lower=0.0)
+    max_tick = ax[0].get_xticks().max()
+    ax[0].set_xticks(np.linspace(min_tick, max_tick, 6))
+    fig.subplots_adjust(hspace=0)
+    render_plot(8, 6, live, 'jn8-club-{0}.png'.format(zero))
+
+def plot_times_by_team(df, live, zero):
+    """Plot the times for each team.
+
+    If zero is True, then set first x-tick at 0 seconds (beginning of
+    race).
+
+    This is almost identical to plot_times_by_club.  They should be the
+    same function."""
+    times = df.loc[:, ['time', 'team']]
+    teams = times.team.unique()
+    fig, ax = plt.subplots(len(teams), sharex=True, sharey=True)
+    for index, team in enumerate(teams):
+        these_times = times[times.team == team]
+        ax[index].plot(these_times.time.as_matrix(), np.ones(len(these_times)), 'o')
+        ax[index].set_ylabel(team, rotation='horizontal', labelpad=30)
+        ax[index].set_yticks([])
+        ax[index].set_ybound(0.5, 1.5)
+        ax[index].set_position([0.1, 0.1, 8.0, 1.0])
+        ax[index].set_yticks([])
+    ax[0].set_title('Temp vu par équipe')
+    min_tick = ax[0].get_xticks().min()
+    if zero:
+        min_tick = 0.0
+        ax[0].set_xbound(lower=0.0)
+    max_tick = ax[0].get_xticks().max()
+    ax[0].set_xticks(np.linspace(min_tick, max_tick, 6))
+    fig.subplots_adjust(hspace=0)
+    render_plot(8, 6, live, 'jn8-team-{0}.png'.format(zero))
+
+def render_plot(xdim, ydim, live, filename):
+    """Display or save the plot image.
+
+    If live is True, then display in a window.
+    If live is False, save to the named file."""
+    if live:
+        plt.show()
+    else:
+        gc = plt.gcf()
+        gc.set_size_inches(xdim, ydim)
+        gc.savefig(filename, dpi=100)
+
+def main():
+    """Fetch data and make some plots."""
+    if len(sys.argv) > 1 and sys.argv[1] == 'live':
+        live = True
+    else:
+        live = False
+    df = get_data()
+    plot_by_club(df, live)
+    plot_by_team(df, live)
+    plot_times_by_club(df, live, True) 
+    plot_times_by_club(df, live, False) 
+    plot_times_by_team(df, live, True) 
+    plot_times_by_team(df, live, False) 
+
+if __name__ == "__main__":
+    main()
