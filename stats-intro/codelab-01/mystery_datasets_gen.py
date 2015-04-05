98121674308e07e3d96e8426e973d5f5fa15bc46
Jeff Abrahamson <jeff@purple.com>
2015-04-05 15:24:18 +0200

    Add dataset c1, c2.

--- stats-intro/codelab-01/mystery_datasets_gen.py
+++ stats-intro/codelab-01/mystery_datasets_gen.py
@@ -3,3 +3,6 @@
 b1 = ss.norm.rvs(5, 3, size=300)
 b2 = ss.norm.rvs(5, 4, size=300)
 b3 = ss.norm.rvs(5.1, 3, size=300)
+
+c1 = ss.norm.rvs(loc=5, scale=3, size=300)
+c2 = np.array([ss.norm.rvs(loc=2, scale=1, size=150), ss.norm.rvs(loc=8, scale=1, size=150)]).reshape(300)
