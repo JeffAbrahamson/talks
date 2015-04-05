0e9dfbf690ffe7e7f8513348c9eba773bba72887
Jeff Abrahamson <jeff@purple.com>
2015-04-05 15:03:16 +0200

    Some data to play with.

---
+++ stats-intro/codelab-01/mystery_datasets_gen.py
@@ -0,0 +1,5 @@
+# a1 and a2 are hand-crafted, copied from some randome web page with numbers on it.
+
+b1 = ss.norm.rvs(5, 3, size=300)
+b2 = ss.norm.rvs(5, 4, size=300)
+b3 = ss.norm.rvs(5.1, 3, size=300)
