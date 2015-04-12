9a964680fefaa8211cac61680805aa7f428b2f73
Jeff Abrahamson <jeff@purple.com>
2015-04-12 10:17:59 +0200

    Flesh out spam example.

--- stats-intro/codelab-06/02_dataframe.py
+++
@@ -1,8 +0,0 @@
-from pandas import DataFrame
-
-def build_data_frame(path, classification):
-    data_frame = DataFrame({'text': [], 'class': []})
-    for file_name, text in read_files(path):
-        data_frame = data_frame.append(
-                DataFrame({'text': [text], 'class': [classification]}, index=[file_name]))
-    return data_frame
