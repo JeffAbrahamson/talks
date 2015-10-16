d86a25a9cd5907678bfc215a97bb76c2ffe1eb9a
Jeff Abrahamson <jeff@purple.com>
2015-10-16 15:59:24 +0200

    Tokenization

---
+++ 2015-11__ML/03-features/one_hot.py
@@ -0,0 +1,13 @@
+>>> from sklearn.feature_extraction import \
+    DictVectorizer
+>>> onehot_encoder = DictVectorizer()
+>>> instances = [
+>>>
+|{'city': 'New York'},|
+>>>
+|{'city': 'San Francisco'},|
+>>>
+|{'city': 'Chapel Hill'}>>> ]|
+>>> print onehot_encoder.fit_transform(instances) \
+                        .toarray()
+|[[ 0. 1. 0.] [ 0. 0. 1.][ 1. 0. 0.]]|
