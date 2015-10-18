6be15801285dbf4fcc3e07d705ea5152e3b6277f
Jeff Abrahamson <jeff@purple.com>
2015-10-18 11:51:19 +0200

    Clean up log reg

---
+++ 2015-11__ML/04-logistic-regression/confusion_matrix_solution.py
@@ -0,0 +1,10 @@
+confusion = confusion_matrix(predictions, y_test)
+print(confusion)
+invert_colors = np.ones(confusion.shape) * confusion.max()
+plt.matshow(invert_colors - confusion)
+plt.title('Confusion matrix')
+plt.gray()
+plt.ylabel('True label')
+plt.xlabel('Predicted label')
+plt.show()
+
